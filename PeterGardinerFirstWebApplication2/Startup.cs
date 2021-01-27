ca7be115aae47e5860c9bb95dca0fe0773470857
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-01-27 12:55:44 -0700

    MVC

--- PeterGardinerFirstWebApplication2/Startup.cs
+++ PeterGardinerFirstWebApplication2/Startup.cs
@@ -16,14 +16,25 @@ namespace PeterGardinerFirstWebApplication2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseNodeModules();
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "Home", action = "Index" });
+            }
+            
+            );
         }
     }
 }
