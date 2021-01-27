38152d9250cd6b3f30246c5dffeaaa6858c99490
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-01-27 15:46:37 -0700

    model validators

--- PeterGardinerFirstWebApplication2/Models/Class.cs
+++ PeterGardinerFirstWebApplication2/Models/Class.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace PeterGardinerFirstWebApplication2.Models
 {
     public class Class
     {
+        [Required]
         public int assignment { get; set; }
+        [Range(0,100)]
         public int quiz { get; set; }
+        [Range(0, 100)]
         public int exams { get; set; }
+        [Range(0, 100)]
         public int intex { get; set; }
+        [Range(0, 100)]
 
 
     }
