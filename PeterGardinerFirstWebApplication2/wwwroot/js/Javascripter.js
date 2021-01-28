ecae83d6a906903bbcce2098c1914c60112f1e02
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-01-27 17:00:20 -0700

    untest

--- PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
+++ PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
@@ -1,6 +1,62 @@
 ﻿$("#submitButton").click(function () {
 
-    alert('test');
+    let assignment = $("#assignment").val();
+    let project = $("#project'").val();
+    let quiz = $("#quiz'").val();
+    let exam = $("#exams").val();
+    let intex = $("#intex").val();
+    let grade = " ";
+
+    //find weighted %
+    assignmentW = assignment * .5;
+    projectW = project * .1;
+    quizW = quiz * .1;
+    examW = exam * .2;
+    intexW = intex * .1;
+
+    //add to find total
+    total = assignmentW + projectW + quizW + examW + intexW;
+
+    //if to calculate letter grade
+    if (total >= 94) {
+        grade = 'A';
+    }
+    else if (total < 94 && total >= 90) {
+        grade = 'A-';
+    }
+    else if (total < 90 && total >= 87) {
+        grade = 'B+';
+    }
+    else if (total < 87 && total >= 84) {
+        grade = 'B';
+    }
+    else if (total < 84 && total >= 80) {
+        grade = 'B-';
+    }
+    else if (total < 80 && total >= 77) {
+        grade = 'C+';
+    }
+    else if (total < 77 && total >= 74) {
+        grade = 'C';
+    }
+    else if (total < 74 && total >= 70) {
+        grade = 'C-';
+    }
+    else if (total < 70 && total >= 67) {
+        grade = 'D+';
+    }
+    else if (total < 67 && total >= 64) {
+        grade = 'D';
+    }
+    else if (total < 64 && total >= 60) {
+        grade = 'D-';
+    }
+    else {
+        grade = 'E';
+    }
+
+    //display letter grade
+    alert('You got an ' + grade + ' with ' + total + '%');
 
 
 });
