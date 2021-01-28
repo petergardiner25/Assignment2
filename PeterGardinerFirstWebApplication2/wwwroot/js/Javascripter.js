9ac0378d727a78647cd395f01eb5df95b946c377
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-01-27 17:13:46 -0700

    anatha

--- PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
+++ PeterGardinerFirstWebApplication2/wwwroot/js/Javascripter.js
@@ -1,4 +1,4 @@
-﻿var submit = $("#submitbutton");
+﻿var submit = document.getElementById("submitbutton");
 
 submit.addEventListener("click", function () {
     let assignment = $("#assignment").val();
